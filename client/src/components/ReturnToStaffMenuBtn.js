e1d4a48bb561a7c9a2c61131fd535db9fdbe0599
Wade Sanchez <wadebsanchez@gmail.com>
2024-04-04 05:04:35 -0400

    edit profile somewhat working

--- client/src/components/ReturnToStaffMenuBtn.js
+++ client/src/components/ReturnToStaffMenuBtn.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import '../index.css'
 
-export const ReturnToStaffMenu = ({className}) => {
+export const ReturnToStaffMenu = ({className, style}) => {
     const navigate = useNavigate();
     const navigateToStaffMenu = () => {
         navigate('/StaffMenu');
         }
   return (
     <>
-            <button className={className} onClick={navigateToStaffMenu}>Return to Staff Menu</button>
+            <button className={className} style={style} onClick={navigateToStaffMenu}>Return to Staff Menu</button>
     </>
   )
 }
