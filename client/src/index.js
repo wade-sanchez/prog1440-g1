0d013e6f0499543c698ef10e5fa416e321402953
Preeti5555 <preetimaan555@gmail.com>
2024-04-04 09:00:48 -0400

    remove the home drop down and little changes.

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import { BrowserRouter } from 'react-router-dom';
+
+
 
 // const router = createBrowserRouter([]);
 
@@ -18,6 +19,7 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
